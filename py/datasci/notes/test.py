393a07da3bf16ebcec330bb24b6aeb9108814793
LanceRemigio <lancejairo.remigio99@gmail.com>
2023-10-08 14:53:59 -0700

    more stuff

--- py/datasci/notes/test.py
+++ py/datasci/notes/test.py
@@ -4,45 +4,21 @@ import matplotlib.pyplot as plt
 
 # make sure you have .show() for every plot you want to display as output
 
-x = np.linspace(0, 5, 11)
+x = np.linspace(0,5, 11)
+
 y = x ** 2
 
-# plt.plot(x,y)
+fig, axes = plt.subplots(1 , 2)
 
-# plt.xlabel('X Label')
-# plt.ylabel('Y label')
-# plt.title('Title')
+axes[0].plot(x,y)
+axes[0].set_title('First Plot')
 
-# plt.subplot(1,2,1)
-# plt.plot(x,y, 'r')
-
-# plt.subplot(1,2,2)
-# plt.plot(y,x, 'b')
+axes[1].plot(y,x)
+axes[1].set_title('Second Plot')
 
 
-
-fig = plt.figure()
-
-# axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
-# axes.plot(x,y)
-
-# axes.set_xlabel('X Label')
-# axes.set_ylabel('Y Label')
-# axes.set_title('Set Title')
-
-
-
-axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3])
-
-axes1.plot(x,y)
-axes1.set_title('LARGER PLOT')
-
-axes2.plot(y,x)
-axes2.set_title('SMALLER PLOT')
+plt.tight_layout()
 
 plt.show()
-
 
 
